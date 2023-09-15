806134ee92687ccc0a099744ebe3bb1efdf49d79
Christian Pisani Thorvik <christian96-th@hotmail.com>
2023-09-15 13:41:02 +0200

    add login for one user

--- viteapp/my-app/src/App.jsx
+++ viteapp/my-app/src/App.jsx
@@ -1,35 +1,40 @@
-import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
+import {useEffect, useState} from "react";
+import {createClient} from "@supabase/supabase-js";
 
 const supabase = createClient("http://localhost:54321", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0");
 
 let canUpdate = true
 
 function App() {
-    const [countries, setCountries] = useState([]);
-    const [cursors, setCursors] = useState([{ x: 0, y: 0 }]);
-    
-    const [channelUpdate, setChannelUpdate] = useState(false);
-    
-    const userId = 4;
+    const [cursors, setCursors] = useState([{x: 0, y: 0}]);
 
-    useEffect(() => {
+    const signIn = async () => {
+        const {data} = await supabase.auth.signInWithPassword({
+            email: 'christian.thorvik@adventuretech.no',
+            password: '1234',
+        })
+        const {user} = data
+
         window.addEventListener("mousemove", async (e) => {
-            if(!canUpdate) return
-            
-            if(canUpdate) {
+            if (!user.id || !canUpdate) return
+
+            if (canUpdate) {
                 canUpdate = false
-                
-                await supabase.from("mousecursortest").upsert({id: userId, x: e.clientX, y: e.clientY})
+
+                await supabase.from("mousecursortest").upsert({"user_id": user.id, x: e.clientX, y: e.clientY})
             }
 
             window.setTimeout(() => {
                 canUpdate = true
             }, 50)
         });
+    }
+
+    useEffect(() => {
+        signIn()
     }, []);
 
-    const channel = supabase
+    supabase
         .channel('schema-db-changes')
         .on(
             'postgres_changes',
@@ -37,29 +42,20 @@ function App() {
                 event: '*',
                 schema: 'public',
             },
-            (payload) => {
-                const cursors = supabase.from("mousecursortest").select("*").then(({data}) => {
-                    setCursors(data.map(cursor => {return {x: cursor.x, y: cursor.y}}))
+            () => {
+                supabase.from("mousecursortest").select("*").then(({data}) => {
+                    setCursors(data.map(cursor => {
+                        return {x: cursor.x, y: cursor.y}
+                    }))
                 })
-                
-                /*if(payload.table === "mousecursortest") {
-                    if(payload.new.id === userId) {
-                        setCursorPosition({x: payload.new.x, y: payload.new.y})
-                    }
-                }*/
             }
         )
         .subscribe()
 
     return (
         <>
-        <ul>
-            {countries.map((country) => (
-                <li key={country.name}>{country.name}</li>
-            ))}
-        </ul>
-            {cursors.map(cursor => 
-                <div style={{
+            {cursors.map((cursor, index) =>
+                <di key={index} style={{
                     backgroundColor: "red",
                     transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
                     width: "20px",
@@ -68,10 +64,10 @@ function App() {
                     position: "fixed",
                     top: 0,
                     left: 0
-                }}></div>
+                }}/>
             )}
 
-            
+
         </>
     );
 }
