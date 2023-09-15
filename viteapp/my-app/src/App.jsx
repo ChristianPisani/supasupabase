56644ffd357daccedf149206c395f13f114e2dc8
Christian Pisani Thorvik <christian96-th@hotmail.com>
2023-09-15 11:27:21 +0200

    mousemove throttle

--- viteapp/my-app/src/App.jsx
+++ viteapp/my-app/src/App.jsx
@@ -3,24 +3,35 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient("http://localhost:54321", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0");
 
+let canUpdate = true
+
 function App() {
     const [countries, setCountries] = useState([]);
+    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
     
     const [channelUpdate, setChannelUpdate] = useState(false);
 
     useEffect(() => {
-        getCountries();
-    }, []);
-    
-    useEffect(() => {
-        console.log("channelUpdate", channelUpdate)
-        getCountries()
-    }, [channelUpdate])
+        window.addEventListener("mousemove", async (e) => {
+            if(!canUpdate) return
+            
+            if(canUpdate) {
+                canUpdate = false
 
-    async function getCountries() {
-        const { data } = await supabase.from("countries").select();
-        setCountries(data);
-    }
+                const {data: existingMousePos} = await supabase.from("mousecursortest").select().eq("id", 1)
+
+                if (!existingMousePos.length > 0) {
+                    const {data, error} = await supabase.from("mousecursortest").insert({id: 1, x: 0, y: 0})
+                }
+
+                await supabase.from("mousecursortest").update({x: e.clientX, y: e.clientY}).eq("id", 1)
+            }
+
+            window.setTimeout(() => {
+                canUpdate = true
+            }, 100)
+        });
+    }, []);
 
     const channel = supabase
         .channel('schema-db-changes')
@@ -31,17 +42,24 @@ function App() {
                 schema: 'public',
             },
             (payload) => {
-                setChannelUpdate(!channelUpdate)
+                if(payload.table === "mousecursortest") {
+                    setCursorPosition({x: payload.new.x, y: payload.new.y})
+                }
             }
         )
         .subscribe()
 
     return (
+        <>
         <ul>
             {countries.map((country) => (
                 <li key={country.name}>{country.name}</li>
             ))}
         </ul>
+            <div style={{backgroundColor: "red", transform: `translateX`}}></div>
+            
+            <p>Cursor position: x: {cursorPosition.x} y: {cursorPosition.y}</p>
+        </>
     );
 }
 
