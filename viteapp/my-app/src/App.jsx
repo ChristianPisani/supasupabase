c5d856dd026a9d4dc34949eec38d9904139a4295
Christian Pisani Thorvik <christian96-th@hotmail.com>
2023-09-15 10:21:43 +0200

    test

---
+++ viteapp/my-app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient("http://localhost:54321", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0");
+
+function App() {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        getCountries();
+    }, []);
+
+    async function getCountries() {
+        const { data } = await supabase.from("countries").select();
+        setCountries(data);
+    }
+
+    return (
+        <ul>
+            {countries.map((country) => (
+                <li key={country.name}>{country.name}</li>
+            ))}
+        </ul>
+    );
+}
+
+export default App;
